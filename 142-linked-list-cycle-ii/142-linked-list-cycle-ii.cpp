12c91075d9f7e3c2a41a76eb253fe65dcdee1f20
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-02-07 22:58:20 +0530

    Time: 19 ms (20.96%), Space: 7.5 MB (90.41%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -1,24 +1,31 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode *getcycle(ListNode *head){
-        ListNode *slow = head,*fast = head;
-        while(fast and fast->next){
-            slow = slow->next;
-            fast=fast->next->next;
-            if(slow == fast)break;
-        }
-        return fast;
-    }
     ListNode *detectCycle(ListNode *head) {
-       ListNode *meetpt =  getcycle(head);
-        if(meetpt == nullptr or meetpt->next == nullptr)return nullptr;//cycle not present
-       ListNode *slow =head;
-        while(slow != meetpt){
-            slow = slow->next;
-            meetpt = meetpt->next;
+        if(head==nullptr or head->next==nullptr)return nullptr;
+        
+        ListNode *slow=head;
+        ListNode *fast=head;
+        ListNode *entry=head;
+        while(fast and fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                while(slow!=entry){
+                    slow=slow->next;
+                    entry=entry->next;
+                }
+                return entry;
+            }
         }
         
-        return meetpt;
+        return nullptr;
     }
 };
