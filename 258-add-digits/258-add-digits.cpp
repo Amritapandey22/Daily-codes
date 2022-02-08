aa32f6bfc7b5553fbf52a9dead7a090735c03f95
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-02-08 22:55:28 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,49 +1,51 @@
-// class Solution {
-// public:
-//     int checkDig(int num){
-//         return floor(log10(num) + 1);
-//     }
-    
-//     int calcSum(int num){
-//         int sum=0;
-//         while(num){
-//             sum+=num%10;
-//             num/=10;
-//         }
-        
-//         return sum;
-//     }
-//     int addDigits(int num) {
-//         bool check=true;
-//         int ans=0;
-//         while(1){
-//          ans=calcSum(num);
-//             if(checkDig(ans)==1){
-//                 //check=false;
-//                 return ans;
-//             }else{
-//                 ans=calcSum(ans);
-//             }
-//         }
-        
-//         return -1;
-//     }
-// };
-
 class Solution {
 public:
-    int addDigits(int num) {
-      int sum=0;
-        while(num>9)
-        {
-            while(num)
-            {
-                sum+=(num%10);
-                num/=10;
-            }
-            num=sum;
-            sum=0;
+    int checkDig(int num){
+        return floor(log10(num) + 1);
+    }
+    
+    int calcSum(int num){
+        int sum=0;
+        while(num){
+            sum+=num%10;
+            num/=10;
         }
+        
+        return sum;
+    }
+    int addDigits(int num) {
+        bool check=true;
+        int ans=0;
+        while(num>9){
+         ans=calcSum(num);
+            if(checkDig(ans)==1){
+                check=false;
+                return ans;
+            }else{
+                ans=calcSum(ans);
+            }
+            num=ans;
+            //cout<<ans<<endl;
+        }
+        
         return num;
     }
 };
+
+// class Solution {
+// public:
+//     int addDigits(int num) {
+//       int sum=0;
+//         while(num>9)
+//         {
+//             while(num)
+//             {
+//                 sum+=(num%10);
+//                 num/=10;
+//             }
+//             num=sum;
+//             sum=0;
+//         }
+//         return num;
+//     }
+// };
