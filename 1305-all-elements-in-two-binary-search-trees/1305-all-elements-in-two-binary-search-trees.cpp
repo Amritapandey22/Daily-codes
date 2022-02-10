ff7d8050b5936bf09e9973b0412e468b3080ac8a
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-02-10 14:02:12 +0530

    Time: 176 ms (72.36%), Space: 86.8 MB (13.47%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,31 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     void preorder(TreeNode* root,vector<int>&ans){
         if(!root)return;
-        ans.push_back(root->val);
+        //ans.push_back(root->val);
         
         preorder(root->left,ans);
+        ans.push_back(root->val);
         preorder(root->right,ans);
         
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int>ans;
+        vector<int>ans1,ans2,ans;
         
-        preorder(root1,ans);
-        preorder(root2,ans);
+        preorder(root1,ans1);
+        preorder(root2,ans2);
         
-        sort(ans.begin(),ans.end());
+        int i=0,j=0;
+        while(i<ans1.size() and j<ans2.size()){
+            if(ans1[i]<ans2[j]){
+                
+                ans.push_back(ans1[i++]);
+            }else{
+                
+                ans.push_back(ans2[j++]);
+            }
+        }
+        
+        while(i<ans1.size()){
+             
+             ans.push_back(ans1[i++]);
+        }
+        
+        while(j<ans2.size()){
+             
+             ans.push_back(ans2[j++]);
+        }
+        
         return ans;
     }
 };
