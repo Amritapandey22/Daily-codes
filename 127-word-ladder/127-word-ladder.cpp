aa5c627133397a74f5625ba9de0044b53e06e0c7
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-02-17 23:32:12 +0530

    Time: 1552 ms (9.70%), Space: 27.6 MB (15.60%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        queue<string> todo;
-        todo.push(beginWord);
-        int ladder = 1;
-        while (!todo.empty()) {
-            int n = todo.size();
-            for (int i = 0; i < n; i++) {
-                string word = todo.front();
-                todo.pop();
-                if (word == endWord) {
-                    return ladder;
-                }
-                dict.erase(word);
-                for (int j = 0; j < word.size(); j++) {
-                    char c = word[j];
-                    for (int k = 0; k < 26; k++) {
-                        word[j] = 'a' + k;
-                        if (dict.find(word) != dict.end()) {
-                            todo.push(word);
-                        }
-                     }
-                    word[j] = c;
+        
+        unordered_set<string>st;
+        for(string str:wordList)st.insert(str);
+        
+        if(st.find(endWord)==st.end()) return 0;
+        
+        queue<string>q;
+        q.push(beginWord);
+        int level=1;
+        while(q.size()){
+            int qsize=q.size();
+            while(qsize--){
+                string word=q.front();
+                q.pop();
+                st.erase(word);
+                //cout<<level<<endl;
+                if(word==endWord)return level;
+                for(int j=0;j<word.size();j++){
+                    char ch=word[j];
+                    for(int i=0;i<26;i++){
+                        word[j]='a'+i;
+                        //cout<<
+                        if(st.find(word)!=st.end())q.push(word);
+                    }
+                    word[j]=ch;
                 }
             }
-            ladder++;
+            
+            level++;
         }
+        
         return 0;
     }
 };
