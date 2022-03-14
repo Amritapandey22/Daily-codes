0c5012a9cbec7919dc9514afd989c482b5d46f2d
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-03-14 20:32:23 +0530

    Time: 28 ms (16.91%), Space: 15.8 MB (52.72%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-     unordered_map<int,int>mp;
-        for(int el:nums){
-            mp[el]++;
+        //only two elements can contribute to majority elemenets in this case
+        int val1=nums[0];
+        int count1=1;
+        
+        int val2=nums[0];
+        int count2=0;
+        
+        for(int i=1;i<nums.size();i++){
+            if(val1==nums[i]){
+                count1++;
+            }else if(val2==nums[i]){
+                count2++;
+            }
+           else{
+             if(count1==0){
+                val1=nums[i];
+                count1=1;
+            }else if(count2==0){
+                val2=nums[i];
+                count2=1;
+            }else{
+                count1--;
+                count2--;
+            }
+           } 
         }
         
-        int n=nums.size();
+        count1=0;
+        count2=0;
         vector<int>ans;
         
-        for(auto it:mp){
-            if(it.second>n/3)ans.push_back(it.first);
+        for(int el:nums){
+            if(el==val1){
+                count1++;
+            }
+            else if(el==val2){
+                count2++;
+            }
         }
+       // cout<<val1<<" "<<val2<<endl;
+        
+        if(count1>(nums.size())/3)ans.push_back(val1);
+        if(count2>(nums.size())/3)ans.push_back(val2);
         
         return ans;
     }
